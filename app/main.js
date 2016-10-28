0ce9d655163c129566552ac4f38013b23f4fb57c
toenu23 <fubarphukhew@gmail.com>
2016-10-28 13:08:09 +0200

    global app object

--- app/main.js
+++ app/main.js
@@ -3,6 +3,9 @@ const { app } = require('electron');
 const env = require('./lib/env');
 const log = require('./lib/log');
 const { createWindow } = require('./lib/window');
+const persistence = require('./lib/persistence/persistence');
+const blockchain = require('./lib/blockchain/blockchain');
+const user = require('./lib/user')
 
 app.on('ready', function() {
   const mainWindow = createWindow();
@@ -14,3 +17,25 @@ app.on('window-all-closed', function() {
 
 log.info('Starting application...');
 log.info('Version: ', env.version);
+
+global.Relief = {};
+
+const onPersistenceInit = function(err) {
+  if (err) {
+    log.error(err);
+    process.exit();
+  }
+  global.Relief.db = persistence.db;
+  global.Relief.user = user;
+  blockchain.init(onBlockchainInit);
+};
+
+const onBlockchainInit = function(err) {
+  if (err) {
+    log.error(err);
+    process.exit();
+  }
+  global.Relief.bc = blockchain.bc;
+};
+
+persistence.init(onPersistenceInit);
