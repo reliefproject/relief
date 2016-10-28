265e4e69e5c3c7d27f9e29590d0950923ce30e01
toenu23 <fubarphukhew@gmail.com>
2016-10-28 14:29:52 +0200

    use global obj where needed

--- app/lib/blockchain/blockchain.js
+++ app/lib/blockchain/blockchain.js
@@ -1,64 +1,61 @@
-const env = require('../env');
-const persistence = require('../persistence/persistence');
-const Btc = require('./btc_electrum');
-const Nxt = require('./nxt_nrs');
+module.exports = function(Relief) {
 
-let bc = {
-  btc: {},
-  nxt: {},
-};
+  const Btc = require('./btc_electrum');
+  const Nxt = require('./nxt_nrs');
 
-const init = function(callback) {
-
-  let serverList = {};
-  const onGetDoc = function(err, doc) {
-    if (err) {
-      return callback(err);
-    }
-    doc = !doc
-      ? { servers: { electrum: {}, nxt: {}, }, }
-      : doc;
-
-    let initServers = {};
-    initServers.electrum = Object.keys(doc.servers.electrum).length > 0
-      ? doc.servers.electrum
-      : env.bootstrapServers.electrum;
-    initServers.nxt = Object.keys(doc.servers.nxt).length > 0
-      ? doc.servers.nxt
-      : env.bootstrapServers.nxt;
-    bc.btc = new Btc(initServers.electrum);
-    bc.nxt = new Nxt(initServers.nxt);
-    bc.btc.getServerList(onGetElectrumServerList);
+  let bc = {
+    btc: {},
+    nxt: {},
   };
 
-  const onGetElectrumServerList = function(err, list) {
-    if (err) {
-      return callback(err);
-    }
-    serverList.electrum = list.electrum;
-    bc.nxt.getServerList(onGetNxtServerList);
+  this.init = function(callback) {
+
+    let serverList = {};
+    const onGetDoc = function(err, doc) {
+      if (err) {
+        return callback(err);
+      }
+      doc = !doc
+        ? { servers: { electrum: {}, nxt: {}, }, }
+        : doc;
+
+      let initServers = {};
+      initServers.electrum = Object.keys(doc.servers.electrum).length > 0
+        ? doc.servers.electrum
+        : Relief.env.bootstrapServers.electrum;
+      initServers.nxt = Object.keys(doc.servers.nxt).length > 0
+        ? doc.servers.nxt
+        : Relief.env.bootstrapServers.nxt;
+      bc.btc = new Btc(initServers.electrum);
+      bc.nxt = new Nxt(initServers.nxt);
+      bc.btc.getServerList(onGetElectrumServerList);
+    };
+
+    const onGetElectrumServerList = function(err, list) {
+      if (err) {
+        return callback(err);
+      }
+      serverList.electrum = list.electrum;
+      bc.nxt.getServerList(onGetNxtServerList);
+    };
+
+    const onGetNxtServerList = function(err, list) {
+      if (err) {
+        return callback(err);
+      }
+      serverList.nxt = list.nxt;
+      Relief.persistence.db.servers.upsert(serverList, onUpdateServerList);
+    };
+
+    const onUpdateServerList = function(err) {
+      if (err) {
+        return callback(err);
+      }
+      callback();
+    };
+
+    Relief.persistence.db.app.getDoc(onGetDoc);
   };
 
-  const onGetNxtServerList = function(err, list) {
-    if (err) {
-      return callback(err);
-    }
-    serverList.nxt = list.nxt;
-    persistence.db.servers.updateDoc(serverList, onUpdateServerList);
-  };
-
-  const onUpdateServerList = function(err) {
-    if (err) {
-      return callback(err);
-    }
-    callback();
-  };
-
-  persistence.init(function() {
-    persistence.db.app.getDoc(onGetDoc);
-  });
-};
-
-module.exports = {
-  init: init,
+  return this;
 };
--- test/blockchain.spec.js
+++ test/blockchain.spec.js
@@ -1,11 +1,20 @@
 const assert = require('assert');
-const blockchain = require('../app/lib/blockchain/blockchain');
+const Blockchain = require('../app/lib/blockchain/blockchain');
+
+const App = {
+  env: require('../app/lib/env'),
+  log: require('../app/lib/log'),
+  window: require('../app/lib/window'),
+  persistence: require('../app/lib/persistence/persistence'),
+};
 
 describe('blockchain', function() {
   it('inits without error', function(done) {
-    blockchain.init(function(err) {
-      assert.equal(err, undefined);
-      done();
+    App.persistence.init(function(err) {
+      Blockchain(App).init(function(err) {
+        assert.equal(err, undefined);
+        done();
+      });
     });
   });
 });
