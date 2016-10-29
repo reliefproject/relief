71642eee42867d58e60bfdad675e39bb2c5bcca3
toenu23 <fubarphukhew@gmail.com>
2016-10-29 08:58:17 +0200

    fix user to use global obj

--- app/lib/user.js
+++ app/lib/user.js
@@ -1,96 +1,94 @@
-const crypto = require('crypto');
-const uuid = require('node-uuid');
-const env = require('./env')
-const persistence = require('./persistence/persistence');
+module.exports = function(Relief) {
 
-const getHash = function(password, salt) {
-  return crypto.pbkdf2Sync(
-    password,
-    salt,
-    env.pbkdf2Iterations,
-    env.pbkdf2Keylen,
-    env.pbkdf2Digest
-  );
-};
+  const crypto = require('crypto');
+  const uuid = require('node-uuid');
+  const env = require('./env')
+  const persistence = require('./persistence/persistence');
 
-const login = function(username, password, callback) {
-
-  const onGetDoc = function(err, doc) {
-    if (err) {
-      return callback(err);
-    }
-    if (!doc.users[username]) {
-      return callback(new Error('Unknown user'));
-    }
-    const salt = doc.users[username].salt;
-    const key = getHash(password, salt);
-    persistence.initUserDb(username, key, onInitUserDb);
+  const getHash = function(password, salt) {
+    return crypto.pbkdf2Sync(
+      password,
+      salt,
+      env.pbkdf2Iterations,
+      env.pbkdf2Keylen,
+      env.pbkdf2Digest
+    );
   };
 
-  const onInitUserDb = function(err) {
-    if (err) {
-      return callback(err);
-    }
-    callback();
-  };
+  this.login = function(username, password, callback) {
 
-  persistence.db.app.getDoc(onGetDoc);
-};
-
-const logout = function(callback) {
-  persistence.unsetUserDb(callback);
-};
-
-const isLoggedIn = function() {
-  return Object.keys(persistence.db.user).length > 0;
-};
-
-const createAccount = function(userData, callback) {
-  let appData = {};
-  const salt = uuid.v4();
-
-  const onGetDoc = function(err, doc) {
-    if (err) {
-      return callback(err);
-    }
-    appData = doc;
-    if (!appData.users) {
-      appData.users = {};
-    }
-    if (appData.users[userData.username] !== undefined) {
-      return callback(new Error('User already exists'));
-    }
-
-    const key = getHash(userData.password, salt);
-    persistence.createUserDb(userData.username, key, onCreateUserDb);
-  };
-
-  const onCreateUserDb = function(err) {
-    if (err) {
-      return callback(err);
-    }
-    const user = {
-      username: userData.username,
-      salt: salt,
+    const onGetDoc = function(err, doc) {
+      if (err) {
+        return callback(err);
+      }
+      if (!doc.users[username]) {
+        return callback(new Error('Unknown user'));
+      }
+      const salt = doc.users[username].salt;
+      const key = getHash(password, salt);
+      persistence.initUserDb(username, key, onInitUserDb);
     };
-    appData.users[userData.username] = user;
-    persistence.db.app.updateDoc(appData, onUpdateDoc);
+
+    const onInitUserDb = function(err) {
+      if (err) {
+        return callback(err);
+      }
+      callback();
+    };
+
+    persistence.db.app.getDoc(onGetDoc);
   };
 
-  const onUpdateDoc = function(err) {
-    if (err) {
-      return callback(err);
-    }
-    delete userData.password;
-    persistence.db.user.insertDoc(userData, callback);
+  this.logout = function(callback) {
+    persistence.unsetUserDb(callback);
   };
 
-  persistence.db.app.getDoc(onGetDoc);
-};
+  this.isLoggedIn = function() {
+    return Object.keys(persistence.db.user).length > 0;
+  };
 
-module.exports = {
-  login: login,
-  logout: logout,
-  isLoggedIn: isLoggedIn,
-  createAccount, createAccount,
+  this.createAccount = function(userData, callback) {
+    let appData = {};
+    const salt = uuid.v4();
+
+    const onGetDoc = function(err, doc) {
+      if (err) {
+        return callback(err);
+      }
+      appData = doc;
+      if (!appData.users) {
+        appData.users = {};
+      }
+      if (appData.users[userData.username] !== undefined) {
+        return callback(new Error('User already exists'));
+      }
+
+      const key = getHash(userData.password, salt);
+      persistence.createUserDb(userData.username, key, onCreateUserDb);
+    };
+
+    const onCreateUserDb = function(err) {
+      if (err) {
+        return callback(err);
+      }
+      const user = {
+        username: userData.username,
+        salt: salt,
+      };
+      appData.users[userData.username] = user;
+      persistence.db.app.updateDoc(appData, onUpdateDoc);
+    };
+
+    const onUpdateDoc = function(err) {
+      if (err) {
+        return callback(err);
+      }
+      delete userData.password;
+      persistence.db.user.insertDoc(userData, callback);
+    };
+
+    persistence.db.app.getDoc(onGetDoc);
+  };
+
+  return this;
 };
