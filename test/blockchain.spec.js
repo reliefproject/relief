d781b79e43d8d01a985b9aac313127c619505902
toenu23 <fubarphukhew@gmail.com>
2016-11-01 17:14:16 +0100

    fix tests

--- test/blockchain.spec.js
+++ test/blockchain.spec.js
@@ -1,12 +1,15 @@
 const assert = require('assert');
+const events = require('events');
 const Blockchain = require('../app/lib/blockchain/blockchain');
 
 describe('blockchain', function() {
   it('inits without error', function(done) {
-    const blockchain = Blockchain();
+    const blockchain = Blockchain({
+      events: new events.EventEmitter(),
+    });
     blockchain.init();
-    assert.equal(typeof blockchain.btc.request, 'function');
-    assert.equal(typeof blockchain.nxt.request, 'function');
+    assert.equal(typeof blockchain.btc.client, 'object');
+    assert.equal(typeof blockchain.nxt.client, 'object');
     done();
   });
 });
