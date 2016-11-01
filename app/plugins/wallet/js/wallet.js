7efd7c5f21ed4cd33978386c62796641b4df2e34
toenu23 <fubarphukhew@gmail.com>
2016-11-01 21:16:17 +0100

    wallet ground work

---
+++ app/plugins/wallet/js/wallet.js
@@ -0,0 +1,31 @@
+(function() {
+
+  const app = angular.module(
+    'Wallet',
+  );
+
+  app.controller('MainCtrl', function($scope, $timeout) {
+
+    let appData;
+    $scope.strings = {};
+
+    Relief.persistence.db.app.getDoc(function(err, data) {
+      if (err) {
+        return Relief.log.error(err);
+      }
+      if (data) {
+        appData = data;
+      }
+
+      // Get data
+    });
+
+    Relief.i18n.loadStrings(language, function(err, strings) {
+      if (err) {
+        return Relief.log.error(err);
+      }
+    });
+
+  });
+
+})();
