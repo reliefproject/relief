3f70c438f2e6a39f297fc1fa338d3c63acb99126
toenu23 <fubarphukhew@gmail.com>
2016-11-02 12:35:15 +0100

    add address modal

--- app/plugins/wallet/js/wallet.js
+++ app/plugins/wallet/js/wallet.js
@@ -14,6 +14,32 @@
       $scope.page = page;
     }
 
+    $scope.forms = {
+      createAddress: {
+        step: 1,
+        type: 'btc',
+        category: { icon: 'icon-record' },
+        label: '',
+        passphrase: '',
+      },
+    };
+
+
+    $scope.addressCategories = [
+      { name: 'default', icon: 'icon-record' },
+      { name: 'red', icon: 'icon-red' },
+      { name: 'blue', icon: 'icon-blue' },
+      { name: 'orange', icon: 'icon-orange' },
+      { name: 'green', icon: 'icon-green' },
+      { name: 'personal', icon: 'icon-user' },
+      { name: 'public', icon: 'icon-megaphone' },
+      { name: 'work', icon: 'icon-briefcase' },
+      { name: 'trading', icon: 'icon-chart-area' },
+      { name: 'favourite', icon: 'icon-heart' },
+      { name: 'shopping', icon: 'icon-basket' },
+      { name: 'rewards', icon: 'icon-trophy' },
+    ];
+
     //$timeout(function() { $scope.page = 'keys'; }, 5000)
     /*
         Relief.persistence.db.app.getDoc(function(err, data) {
