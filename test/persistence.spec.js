4a2742aeb49dbd6c5f722e14cf94ee06ed622119
toenu23 <fubarphukhew@gmail.com>
2016-11-07 11:53:52 +0100

    fix tests

--- test/persistence.spec.js
+++ test/persistence.spec.js
@@ -37,7 +37,7 @@ describe('user db', function() {
     });
   });
   it('can write into user db', function(done) {
-    persistence.db.user.updateDoc({ food: 'sandwich' }, function(err) {
+    persistence.db.user.update({ food: 'sandwich' }, function(err) {
       assert.equal(err, undefined);
       done();
     });
