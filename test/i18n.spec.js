b89ad1e164caa0b332798a96e18cf76c03bb828f
toenu23 <fubarphukhew@gmail.com>
2016-11-07 07:57:48 +0100

    fix i18n test

--- test/i18n.spec.js
+++ test/i18n.spec.js
@@ -3,14 +3,14 @@ const i18n = require('../app/lib/i18n');
 
 describe('i18n', function() {
   it('loads the translation strings', function(done) {
-    i18n.loadStrings('en', function(err, strings) {
+    i18n.loadStrings('en', 'app', function(err, strings) {
       assert.equal(err, undefined);
       assert.equal(strings['app'].APP_NAME, 'Relief');
       done();
     });
   });
   it('does not load translation strings', function(done) {
-    i18n.loadStrings('yxz', function(err, strings) {
+    i18n.loadStrings('yxz', 'abc', function(err, strings) {
       assert((err instanceof Error), true);
       done();
     });
