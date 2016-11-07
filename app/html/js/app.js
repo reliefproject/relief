3b2bca94e96aa6977fbf7a881d7f8f1625e24405
toenu23 <fubarphukhew@gmail.com>
2016-11-07 11:43:39 +0100

    webviewinit

--- app/html/js/app.js
+++ app/html/js/app.js
@@ -157,3 +157,19 @@ app.controller(
     });
   },
 ]);
+
+
+app.directive('webviewinit', function($timeout) {
+  return {
+    link: function(scope, element, attrs) {
+      element.bind('did-finish-load', function(e) {
+        this.addEventListener('console-message', function(e) {
+          Relief.log.info(e.message);
+        });
+        if (Relief.env.name === 'development') {
+          this.openDevTools();
+        }
+      });
+    },
+  }
+});
