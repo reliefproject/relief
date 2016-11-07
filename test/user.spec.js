c7753e4164f2ba27a9eabf5969e51c19095754a7
toenu23 <fubarphukhew@gmail.com>
2016-11-07 13:30:37 +0100

    remove test

--- test/user.spec.js
+++ test/user.spec.js
@@ -30,49 +30,6 @@ describe('user', function() {
     });
 
   });
-  it('gets the users balances', function(done) {
-    let addresses = [
-      {
-        type: 'btc',
-        address: '1NS17iag9jJgTHD1VXjvLCEnZuQ3rJDE9L',
-      },
-      {
-        type: 'btc',
-        address: '1EZBqbJSHFKSkVPNKzc5v26HA6nAHiTXq6',
-      },
-      {
-        type: 'btc',
-        address: '1MDUVf2dRJ6wpam9vLyju3GWPAUPeGgQ6S',
-      },
-      {
-        type: 'btc',
-        address: '19N2f7xTHCVjXsdL3aGQPFFm8MZYmxchMX',
-      },
-      {
-        type: 'nxt',
-        address: 'NXT-DXWC-NADK-MCCZ-EZK8A',
-      },
-      {
-        type: 'nxt',
-        address: 'NXT-7LB8-8ZPX-3YR9-3L85B',
-      },
-      {
-        type: 'nxt',
-        address: 'NXT-MHJ2-E6M3-H5FY-2NCQ7',
-      },
-      {
-        type: 'nxt',
-        address: ' NXT-WSHC-U9Z4-BKN6-543CS',
-      },
-    ];
-    persistence.db.user.update({ addresses: addresses }, function(err, data) {
-      user.getBalances(function(err, data) {
-        // TODO better
-        assert.equal(err, undefined);
-        done();
-      });
-    });
-  });
   it('logs out', function(done) {
     user.logout(function(err) {
       const isLoggedIn = user.isLoggedIn();
