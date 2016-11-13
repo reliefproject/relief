62ae6e0a4f589fd33edd5d1ed4332dd6e92a039d
toenu23 <fubarphukhew@gmail.com>
2016-11-13 20:11:43 +0100

    Console message log levels

--- app/html/js/directives/webviewinit.js
+++ app/html/js/directives/webviewinit.js
@@ -5,7 +5,15 @@
       link: function(scope, element, attrs) {
         element.bind('did-finish-load', function(e) {
           this.addEventListener('console-message', function(e) {
-            Relief.log.info(e.message);
+            if (e.level === 0) {
+              Relief.log.info(e.message);
+            }
+            if (e.level === 1) {
+              Relief.log.warn(e.message);
+            }
+            if (e.level === 2) {
+              Relief.log.error(e.message);
+            }
           });
           if (Relief.env.name === 'development') {
             this.openDevTools();
