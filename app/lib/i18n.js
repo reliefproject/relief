dd6d3f6a2dd5117bfbfc59e44a6fe36183becf50
toenu23 <fubarphukhew@gmail.com>
2016-11-14 12:27:23 +0100

    Common i18n file

--- app/lib/i18n.js
+++ app/lib/i18n.js
@@ -2,31 +2,41 @@
 
   const path = require('path');
   const jetpack = require('fs-jetpack');
+  const log = require('./log');
   const i18nDir = path.join(__dirname, '..', 'common', 'i18n');
 
 
   const loadStrings = function(lang, filename) {
     return new Promise(function(resolve, reject) {
+
       const msgDir = path.join(i18nDir, lang);
       if (!jetpack.exists(msgDir)) {
         return reject('Unknown language');
       }
-      const file = path.join(i18nDir, lang, filename + '.json');
+      const file = path.join(msgDir, filename + '.json');
       if (!jetpack.exists(file)) {
         return reject('File not found');
       }
+
+      let strings = {};
+      strings = jetpack.read(
+        path.join(msgDir, 'common.json'),
+        'json'
+      );
       const content = JSON.parse(
         jetpack.read(file)
       );
-      if (Object.keys(content).length === 0) {
-        return reject('No translations found');
-      }
-      resolve(content);
+      Object.assign(strings, content);
+
+      resolve(strings);
+
     });
   };
+
 
   module.exports = {
     loadStrings: loadStrings,
   };
+
 
 })();
