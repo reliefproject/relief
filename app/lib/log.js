35c5313355cea608539dd4a64bc643f82b9d6dc6
toenu23 <fubarphukhew@gmail.com>
2016-11-14 13:38:09 +0100

    Delete old log files

--- app/lib/log.js
+++ app/lib/log.js
@@ -1,21 +1,29 @@
 (function() {
 
+
   const path = require('path');
   const { app } = require('electron');
   const winston = require('winston');
   const moment = require('moment');
   const jetpack = require('fs-jetpack');
+  const findRemoveSync = require('find-remove')
   const env = require('./env.js');
 
-  const now = moment().format('YYYYMMDD-hhmm');
-  const filename = env.logFilePrefix + now + env.logFileSuffix;
-  const logFile = path.join(
+
+  const today = moment().format('YYYYMMDD');
+  const filename = env.logFilePrefix + today + env.logFileSuffix;
+  const logsDir = path.join(
     app.getPath('userData'),
-    env.logsDir,
+    env.logsDir
+  );
+  const logFile = path.join(
+    logsDir,
     filename
   );
 
+
   jetpack.file(logFile);
+
 
   const logger = new (winston.Logger)({
     transports: [
@@ -29,6 +37,26 @@
     ],
   });
 
+
+  // Clean up log dir
+  const options = {
+    age: {
+      seconds: (env.deleteLogsOlderThanDays * 86400)
+    },
+    extensions: env.logFileSuffix
+  };
+  const result = findRemoveSync(
+    logsDir,
+    options
+  );
+  for (let k in result) {
+    if (result[k]) {
+      logger.info('Deleted log file', k);
+    }
+  }
+
+
   module.exports = logger;
+
 
 })();
