ed1a369850dd8d5617be8e1c931491d53a727fbd
toenu23 <fubarphukhew@gmail.com>
2016-11-14 12:40:22 +0100

    Fallback to default lang if string is missing

--- app/lib/i18n.js
+++ app/lib/i18n.js
@@ -2,31 +2,36 @@
 
   const path = require('path');
   const jetpack = require('fs-jetpack');
-  const log = require('./log');
+  const env = require('./env');
   const i18nDir = path.join(__dirname, '..', 'common', 'i18n');
 
 
   const loadStrings = function(lang, filename) {
     return new Promise(function(resolve, reject) {
 
-      const msgDir = path.join(i18nDir, lang);
-      if (!jetpack.exists(msgDir)) {
-        return reject('Unknown language');
-      }
-      const file = path.join(msgDir, filename + '.json');
-      if (!jetpack.exists(file)) {
-        return reject('File not found');
-      }
-
       let strings = {};
+      const defaultMsgDir = path.join(i18nDir, env.defaultLanguage);
+      const defaultFile = path.join(defaultMsgDir, filename + '.json');
       strings = jetpack.read(
-        path.join(msgDir, 'common.json'),
+        path.join(defaultMsgDir, 'common.json'),
         'json'
       );
-      const content = JSON.parse(
-        jetpack.read(file)
+      Object.assign(
+        strings,
+        jetpack.read(defaultFile, 'json')
       );
-      Object.assign(strings, content);
+
+      const msgDir = path.join(i18nDir, lang);
+      const file = path.join(msgDir, filename + '.json');
+      const commonFile = path.join(msgDir, 'common.json');
+      Object.assign(
+        strings,
+        jetpack.read(commonFile, 'json')
+      );
+      Object.assign(
+        strings,
+        jetpack.read(file, 'json')
+      );
 
       resolve(strings);
 
